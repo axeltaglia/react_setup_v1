ad82cb53ac889dce209344bcbf0e7129ba42f690
axeltaglia <axeltaglia@gmail.com>
2020-09-20 09:44:40 -0300

    Redux store + Material + Helmet

--- src/App.js
+++ src/App.js
@@ -1,58 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
 
-function App() {
+import Helmet from 'react-helmet';
+
+import DateFnsUtils from "@date-io/date-fns";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { StylesProvider } from "@material-ui/styles";
+import { ThemeProvider } from "styled-components";
+
+import maTheme from "./theme";
+import Routes from "./routes/Routes";
+function App({ theme }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <React.Fragment>
+        <Helmet
+            titleTemplate="%s | React Setup Project"
+            defaultTitle="React Setup Project"
+        />
+        <StylesProvider injectFirst>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <MuiThemeProvider theme={maTheme[theme.currentTheme]}>
+              <ThemeProvider theme={maTheme[theme.currentTheme]}>
+                <Routes />
+              </ThemeProvider>
+            </MuiThemeProvider>
+          </MuiPickersUtilsProvider>
+        </StylesProvider>
+      </React.Fragment>
   );
 }
 
-export default App;
+export default connect(store => ({ theme: {currentTheme: 0} }))(App);
